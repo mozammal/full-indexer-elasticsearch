2f5994f5e7f8b098061211e4e62b16f20aedf71d
mozammal <mozammaltomal.1001@gmail.com>
2020-07-13 11:13:41 +0200

    add deployment script for docker compose
    docker compose script is used to deploy the indexer service with elasticserach and test was carried out on ubuntu 18.04

--- README.md
+++ README.md
@@ -16,12 +16,24 @@ clone the repo with the command given below:
 git clone https://github.com/mozammal/full-indexer-elasticsearch.git
 ```
 
-One way to run this application is to use maven to run  maven from the command line:
+One way to run this application is to use docker-compose from the command line
+(tested on ubuntu 18.04.1):
 
 ```shell
 cd full-indexer-elasticsearch
-mvn spring-boot:run
+mvn clean package
+sudo sysctl -w vm.max_map_count=262144
+sudo docker-compose up -d
 ```
+
+One way to stop this application is to use docker-compose from the command line
+(tested on ubuntu 18.04.1):
+
+```shell
+sudo docker-compose stop
+```
+
+
 
 The REST APIs documentation can be accessed at 
 - [APIs doc link](http://localhost:8080/swagger-ui.html) 
--- src/main/java/com/mozammal/fullindexerelasticsearch/config/RestClientConfig.java
+++ src/main/java/com/mozammal/fullindexerelasticsearch/config/RestClientConfig.java
@@ -1,6 +1,8 @@
 package com.mozammal.fullindexerelasticsearch.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +13,18 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
 @Configuration
 public class RestClientConfig extends AbstractElasticsearchConfiguration {
 
+  Logger LOGGER = LoggerFactory.getLogger(RestClientConfig.class);
+
   @Value("${spring.elasticsearch.rest.uris}")
   private String url;
+
 
   @Override
   @Bean(name = "elasticRestClient")
   public RestHighLevelClient elasticsearchClient() {
+    LOGGER.info("url {}", url);
     final ClientConfiguration clientConfiguration =
-        ClientConfiguration.builder().connectedTo(url.substring(6)).build();
+        ClientConfiguration.builder().connectedTo(url.substring(7)).build();
     return RestClients.create(clientConfiguration).rest();
   }
 }
